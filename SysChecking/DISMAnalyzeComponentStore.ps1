85ac1a54e0a9f0c40d38804c8ba340165c259b3a
dropdb404 <114319973+dropdb404@users.noreply.github.com>
2024-01-06 19:14:44 +0800

    Create DISMAnalyzeComponentStore.ps1

---
+++ SysChecking/DISMAnalyzeComponentStore.ps1
@@ -0,0 +1,47 @@
+﻿$DISMAnalyze = cmd /r 'dism /Online /Cleanup-Image /AnalyzeComponentStore'
+
+if (($DISMAnalyze[$DISMAnalyze.count -14] -split " : ")[0] -like "Windows Explorer Reported Size of Component Store"){
+    $WindowsExplorerReportedSizeofComponentStore=($DISMAnalyze[$DISMAnalyze.count -14] -split " : ")[1]
+}
+
+if (($DISMAnalyze[$DISMAnalyze.count -12] -split " : ")[0] -like "Actual Size of Component Store"){
+    $ActualSizeofComponentStore=($DISMAnalyze[$DISMAnalyze.count -12] -split " : ")[1]
+}
+
+if (($DISMAnalyze[$DISMAnalyze.count -10] -split " : ")[0] -like "*Shared with Windows"){
+    $SharedwithWindows=($DISMAnalyze[$DISMAnalyze.count -10] -split " : ")[1]
+}
+
+if (($DISMAnalyze[$DISMAnalyze.count -9] -split " : ")[0] -like "*Backups and Disabled Features"){
+    $BackupsandDisabledFeatures=($DISMAnalyze[$DISMAnalyze.count -9] -split " : ")[1]
+}
+
+if (($DISMAnalyze[$DISMAnalyze.count -8] -split " : ")[0] -like "*Cache and Temporary Data"){
+    $CacheandTemporaryData=($DISMAnalyze[$DISMAnalyze.count -8] -split " : ")[1]
+}
+
+if (($DISMAnalyze[$DISMAnalyze.count -6] -split " : ")[0] -like "Date of Last Cleanup"){
+    $DateofLastCleanup=($DISMAnalyze[$DISMAnalyze.count -6] -split " : ")[1]
+}
+
+if (($DISMAnalyze[$DISMAnalyze.count -4] -split " : ")[0] -like "Number of Reclaimable Packages"){
+    $NumberofReclaimablePackages=($DISMAnalyze[$DISMAnalyze.count -4] -split " : ")[1]
+}
+
+if (($DISMAnalyze[$DISMAnalyze.count -3] -split " : ")[0] -like "Component Store Cleanup Recommended"){
+    $ComponentStoreCleanupRecommended=($DISMAnalyze[$DISMAnalyze.count -3] -split " : ")[1]
+}
+
+$result=[PSCustomObject]@{
+    Hostname=$env:COMPUTERNAME;
+    WindowsExplorerReportedSizeofComponentStore=$WindowsExplorerReportedSizeofComponentStore;
+    ActualSizeofComponentStore=$ActualSizeofComponentStore;
+    SharedwithWindows=$SharedwithWindows;
+    BackupsandDisabledFeatures=$BackupsandDisabledFeatures;
+    CacheandTemporaryData=$CacheandTemporaryData;
+    DateofLastCleanup=$DateofLastCleanup;
+    ComponentStoreCleanupRecommended=$ComponentStoreCleanupRecommended;
+    NumberofReclaimablePackages=$NumberofReclaimablePackages    
+}
+
+$result
